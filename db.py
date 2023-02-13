99e2b8549609e8f4e8b0ef6c7ce2c9eb544acbd0
Akash More <akashkmore2002@gmail.com>
2023-02-13 23:35:07 +0530

    Chnaged Datbase connections --> Connected to diffrent database service

--- db.py
+++ db.py
@@ -16,13 +16,13 @@ db = mysql.connector.connect(
 cur = db.cursor()
 
 def add_comment(id,uname,comments):
-    query = "INSERT INTO sql12595788.comments_table (content_id, uname, comments) VALUES (%s,%s,%s);"
+    query = "INSERT INTO bgxj2qxoat8r2vkjj0im.comments_table (content_id, uname, comments) VALUES (%s,%s,%s);"
     val = [id,uname,comments]
     cur.execute(query,val)
     db.commit()
 
 def read_comments(content_id):
-    query = "select * from sql12595788.comments_table where content_id = %s;"
+    query = "select * from bgxj2qxoat8r2vkjj0im.comments_table where content_id = %s;"
     cur.execute(query, [content_id])
     data = cur.fetchall()
     data = [list(i) for i in data]
